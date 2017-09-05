1ab9f587ba043381643dd53480a56634ed0c2da0
Unknown <cojamo15@gmail.com>
2017-09-05 10:23:35 -0600

    Updated Files
    I added in the specific contact.php code Hiro wants us to use for this assignment to the php files. We just need to set up the form to speak to this file. I also updated the html files (for all 3 assignments) so that the input field names match what is in the php file we have been given. I also added in a "textarea" input field.

--- 02_jQuery_ajax/contact.php
+++ 02_jQuery_ajax/contact.php
@@ -1,24 +1,18 @@
-<?php
-    //====================================================
-    // Program : Fake send mail
-    // Author : pouletfou at gmail
-    // Description : save this file as /usr/sbin/sendmail
-    //  and you can test your PHP applications using mail
-    //  by looking at the /tmp/fakesendmail.log files.
+<?php 
 
-	//	I AM NOT SURE IF THIS IS THE CORRECT PHP FILE WE SHOULD BE USING?
-    //====================================================
+  $name = $_REQUEST['name'];
+  $from = $_REQUEST['from'];
+  $body = $_REQUEST['body'];
 
-    define('LOGFILE','/tmp/fakesendmail.log');
+  $to = "hiro@mrhiro.com"; //recipient 
 
-    $log = fopen (LOGFILE,'a+');
+  $subject = "RIA Emails"; //subject 
+  $header = "From: ". $name . " <" . $from . ">\r\n";
 
-    fwrite($log,"\n".implode(' ',$argv).
-     " called on : ".date('Y-m-d H:i:s')."\n");
-
-    fwrite($log,file_get_contents("php://stdin"));
-    fwrite($log,
-"\n===========================================================\n");
-    fclose($log);
+  if (mail($to, $subject, $body, $header)){
+    echo 'Your email has been sent!';
+  } else {
+    echo 'Error: something went wrong.';
+  }
 
 ?>
