977e102b8efcd836a1290f2b732c1b397ee7f287
Mike Cowley <Mykie92@mikes-air-346.ad.uvu.edu>
2017-09-06 20:11:54 -0600

    HTML Form FIX!!

--- 02_jQuery_ajax/contact.php
+++ 02_jQuery_ajax/contact.php
@@ -1,18 +1,132 @@
-<?php 
+<?php
 
-  $name = $_REQUEST['name'];
-  $from = $_REQUEST['from'];
-  $body = $_REQUEST['body'];
+    // Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["name"]));
+				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["message"]);
 
-  $to = "hiro@mrhiro.com"; //recipient 
+        // Check that data was sent to the mailer.
+        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+            exit;
+        }
 
-  $subject = "RIA Emails"; //subject 
-  $header = "From: ". $name . " <" . $from . ">\r\n";
+        // Set the recipient email address.
+        // FIXME: Update this to your desired email address.
+        $recipient = "mike-cowley@hotmail.com";
 
-  if (mail($to, $subject, $body, $header)){
-    echo 'Your email has been sent!';
-  } else {
-    echo 'Error: something went wrong.';
-  }
+        // Set the email subject.
+        $subject = "New contact from $name";
+
+        // Build the email content.
+        $email_content = "Name: $name\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Message:\n$message\n";
+
+        // Build the email headers.
+        $email_headers = "From: $name <$email>";
+
+        // Send the email.
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Thank You! Your message has been sent.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
+
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
 
 ?>
+
+
+
+
+
+
+
+//<?php
+//// process.php
+//
+//$errors         = array();      // array to hold validation errors
+//$data           = array();      // array to pass back data
+//
+//// validate the variables ======================================================
+//    // if any of these variables don't exist, add an error to our $errors array
+//
+//    if (empty($_POST['name']))
+//        $errors['name'] = 'Name is required.';
+//
+//    if (empty($_POST['address']))
+//        $errors['address'] = 'Address is required.';
+//
+//    if (empty($_POST['from']))
+//        $errors['from'] = 'Email is required.';
+//
+////    if (empty($_POST['body']))
+////         $errors['body'] = 'Body is required.';
+//
+//// return a response ===========================================================
+//
+//    // if there are any errors in our errors array, return a success boolean of false
+//    if ( ! empty($errors)) {
+//
+//        // if there are items in our errors array, return those errors
+//        $data['success'] = false;
+//        $data['errors']  = $errors;
+//    } else {
+//
+//        // if there are no errors process our form, then return a message
+//        $to("mike-cowley@hotmail.com");
+//        $subject = "AJAX Form Info";
+//        $message = "$name has submitted the AJAX form. $body $address";
+//
+//        mail($to, $subject, $message, 'FROM:'.$email);
+//
+//        // DO ALL YOUR FORM PROCESSING HERE
+//        // THIS CAN BE WHATEVER YOU WANT TO DO (LOGIN, SAVE, UPDATE, WHATEVER)
+//
+//        // show a message of success and provide a true success variable
+//        $data['success'] = true;
+//        $data['message'] = 'Success!';
+//    }
+//
+//    // return all our data to an AJAX call
+//    echo json_encode($data);
+//
+
+
+
+
+
+
+
+//<?php
+//
+//  $name = $_REQUEST['name'];
+//  $from = $_REQUEST['from'];
+//  $body = $_REQUEST['body'];
+//
+//  $to = "mike-cowley@hotmail.com"; //recipient
+//
+//  $subject = "RIA Emails"; //subject
+//  $header = "From: ". $name . " <" . $from . ">\r\n";
+//
+//  if (mail($to, $subject, $body, $header)){
+//    echo 'Your email has been sent!';
+//  } else {
+//    echo 'Error: something went wrong.';
+//  }
+//
+//?>
--- 02_jQuery_ajax/js/main.js
+++ 02_jQuery_ajax/js/main.js
@@ -1,27 +1,214 @@
 //JS file
+//
+// $('form.ajax').on('submit', function(){
+// 	var that = $(this),
+// 		url = that.attr('action'),
+// 		type = that.attr('method'),
+// 		data = {};
+//
+// 	that.find('[name]').each(function(index, value){
+// 		var that = $(this),
+// 			name = that.attr('name'),
+// 			value = that.val();
+//
+// 		data[name] = value;
+// 	});
+// 	console.log(data);
+//
+// 	$.ajax({
+// 		url: url,
+// 		type: type,
+// 		data: data,
+// 		success: function(response){
+// 			console.log(response);
+// 		}
+// 	});
+// 	return false;
+// });
 
-$('form.ajax').on('submit', function(){
-	var that = $(this),
-		url = that.attr('action'),
-		type = that.attr('method'),
-		data = {};
 
-	that.find('[name]').each(function(index, value){
-		var that = $(this),
-			name = that.attr('name'),
-			value = that.val();
 
-		data[name] = value;
-	});
-	console.log(data);
 
-	$.ajax({
-		url: url,
-		type: type,
-		data: data,
-		success: function(response){
-			console.log(response);
+// magic.js
+// $(document).ready(function() {
+//
+//     // process the form
+//     $('form').submit(function(event) {
+//
+//         // get the form data
+//         // there are many ways to get this data using jQuery (you can use the class or id also)
+//         var formData = {
+//             'name'              : $('input[name=name]').val(),
+//             'address'   		: $('input[name=address]').val(),
+//             'from'              : $('input[name=from]').val(),
+//             // 'body'   			: $('input[name=body]').val()
+//         };
+//
+//         // process the form
+//         $.ajax({
+//             type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+//             url         : 'contact.php', // the url where we want to POST
+//             data        : formData, // our data object
+//             dataType    : 'json', // what type of data do we expect back from the server
+//             encode          : true
+//         })
+//         // using the done promise callback
+//             .done(function(data) {
+//
+//                 // log data to the console so we can see
+//                 console.log(data);
+//
+//                 // here we will handle errors and validation messages
+//             });
+//
+//         // stop the form from submitting the normal way and refreshing the page
+//         event.preventDefault();
+//     });
+//
+// });
+
+
+// magic.js
+// $(document).ready(function() {
+
+//     // process the form
+//     $('form').submit(function(event) {
+//
+//         $('.form-group').removeClass('has-error'); // remove the error class
+//         $('.help-block').remove(); // remove the error text
+//
+//         // get the form data
+//         // there are many ways to get this data using jQuery (you can use the class or id also)
+//         var formData = {
+//             'name'              : $('input[name=name]').val(),
+//             'address'   		: $('input[name=address]').val(),
+//             'from'              : $('input[name=from]').val(),
+//             'body'   			: $('input[name=body]').val()
+//         };
+//
+//         // process the form
+//         $.ajax({
+//             type 		: 'POST', // define the type of HTTP verb we want to use (POST for our form)
+//             url 		: 'contact.php', // the url where we want to POST
+//             data 		: formData, // our data object
+//             dataType 	: 'json', // what type of data do we expect back from the server
+//             encode 		: true
+//         })
+//         // using the done promise callback
+//             .done(function(data) {
+//
+//                 // log data to the console so we can see
+//                 console.log(data);
+//
+//                 // here we will handle errors and validation messages
+//                 if ( ! data.success) {
+//
+//                     // handle errors for name ---------------
+//                     if (data.errors.name) {
+//                         $('#name-group').addClass('has-error'); // add the error class to show red input
+//                         $('#name-group').append('<div class="help-block">' + data.errors.name + '</div>'); // add the actual error message under our input
+//                     }
+//
+//                     // handle errors for email ---------------
+//                     if (data.errors.email) {
+//                         $('#address-group').addClass('has-error'); // add the error class to show red input
+//                         $('#address-group').append('<div class="help-block">' + data.errors.email + '</div>'); // add the actual error message under our input
+//                     }
+//
+//                     // handle errors for superhero alias ---------------
+//                     if (data.errors.superheroAlias) {
+//                         $('#email-group').addClass('has-error'); // add the error class to show red input
+//                         $('#email-group').append('<div class="help-block">' + data.errors.superheroAlias + '</div>'); // add the actual error message under our input
+//                     }
+//
+//                 } else {
+//
+//                     // ALL GOOD! just show the success message!
+//                     $('form').append('<div class="alert alert-success">' + data.message + '</div>');
+//
+//                     // usually after form submission, you'll want to redirect
+//                     // window.location = '/thank-you'; // redirect a user to another page
+//
+//                 }
+//             })
+//
+//             // using the fail promise callback
+//             .fail(function(data) {
+//
+//                 // show any errors
+//                 // best to remove for production
+//                 console.log(data);
+//             });
+//
+//         // stop the form from submitting the normal way and refreshing the page
+//         event.preventDefault();
+//     });
+//
+// });
+
+
+$(document).ready(function() {
+
+    $(function () {
+        // Get the form.
+        var form = $('#ajax-contact');
+
+        // Get the messages div.
+        var formMessages = $('#form-messages');
+
+        // TODO: The rest of the code will go here...
+    });
+
+	// Set up an event listener for the contact form.
+    $(form).submit(function(event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+
+        // TODO
+    });
+
+
+    // Serialize the form data.
+    var formData = $(form).serialize();
+
+
+    // Submit the form using AJAX.
+    $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+    })
+
+
+
+	.done(function(response) {
+		// Make sure that the formMessages div has the 'success' class.
+		$(formMessages).removeClass('error');
+		$(formMessages).addClass('success');
+
+		// Set the message text.
+		$(formMessages).text(response);
+
+		// Clear the form.
+		$('#name').val('');
+		$('#email').val('');
+		$('#message').val('');
+	})
+
+
+	.fail(function(data) {
+		// Make sure that the formMessages div has the 'error' class.
+		$(formMessages).removeClass('success');
+		$(formMessages).addClass('error');
+
+		// Set the message text.
+		if (data.responseText !== '') {
+			$(formMessages).text(data.responseText);
+		} else {
+			$(formMessages).text('Oops! An error occured and your message could not be sent.');
 		}
 	});
-	return false;
-});
+
+
+
+};
